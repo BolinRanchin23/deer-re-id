a7f579aaa6b761db4b3e17ebfcdc67a9a5849249
Partners AI <partnersai@Partnerss-MacBook-Pro.local>
2026-08-12 05:59:17 -0500

    Allow immutable HD asset preview paths

--- tests/test_catalog_dashboard.py
+++ tests/test_catalog_dashboard.py
@@ -185,7 +185,7 @@ class MemoryCatalog:
 
     def resolve_media_asset_object(self, media_asset_id):
         self.resolved_asset = media_asset_id
-        return {"object_path": "cam@" + "a" * 64 + "/2026/08/08/20260808T120000Z_hd@" + "c" * 64 + ".jpg", "content_type": "image/jpeg"}
+        return {"object_path": "cam@" + "a" * 64 + "/2026/08/08/20260808T120000Z_12345678-1234-4123-8123-123456789abc-cloud-hd@" + "c" * 64 + "_hd.jpg", "content_type": "image/jpeg"}
 
     def read_private_image(self, object_path, max_bytes):
         self.read_args = (object_path, max_bytes)
